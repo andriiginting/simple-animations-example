2ec11ccf6b303478eb74b0cc0da89c9ba0e77d8d
andriiginting <andri.ginting@go-jek.com>
2020-03-10 23:14:39 +0700

    [Andri] add animator class function to change color

--- app/src/main/java/com/andriiginting/animationsexample/MainActivity.kt
+++ app/src/main/java/com/andriiginting/animationsexample/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.andriiginting.animationsexample
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -8,20 +12,24 @@ import android.view.animation.Animation.INFINITE
 import android.view.animation.Animation.RELATIVE_TO_SELF
 import android.view.animation.Animation.REVERSE
 import android.view.animation.RotateAnimation
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         //rotation animations, call doRotation()
+        //change color animations, call changeViewColor()
+        //change color animations, call changeColorObjectAnimator()
 
-        doRotation(2000)
+        changeColorObjectAnimator()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -48,6 +56,34 @@ class MainActivity : AppCompatActivity() {
             repeatMode = REVERSE
             duration = repeatDurations
         }.let(::applyAnimations)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun changeViewColor() {
+        ValueAnimator.ofArgb(Color.BLACK, Color.YELLOW, Color.GREEN)
+            .apply {
+                duration = 2000
+                repeatCount = ValueAnimator.INFINITE
+                repeatMode = ValueAnimator.REVERSE
+                addUpdateListener { colors ->
+                    tvTextAnimations.setTextColor(colors.animatedValue as Int)
+                }
+                start()
+            }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun changeColorObjectAnimator(){
+        ObjectAnimator.ofArgb(
+            tvTextAnimations,
+            "andriiginting",
+            Color.BLACK, Color.YELLOW, Color.GREEN
+        ).apply {
+            duration = 2000
+            repeatCount = ValueAnimator.INFINITE
+            repeatMode = ValueAnimator.REVERSE
+            start()
+        }
     }
 
     private fun applyAnimations(animations: Animation) {
